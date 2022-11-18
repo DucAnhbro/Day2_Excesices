4efaeb9519f27db793528849dcf344aa9e100aa1
DucAnhbro <luuanhql456123@gmail.com>
2022-11-18 10:11:19 +0700

    update

--- BT2/Program.cs
+++ BT2/Program.cs
@@ -12,22 +12,30 @@ namespace BT2
 
         static void Main(string[] args)
         {
-            int[] Arr = new int[9] { 2, 3, 1, 5, 4, 6, 0, 20, 14 };
-            var Minx = 0;
+            int[] Arr = new int[9] { 2, 3, 1, 5, 4, 6, 8, 20, 14 };
+            var Minx = Int32.MaxValue;
             Find( Arr, ref Minx);
         }
 
+        
         private static void Find(int[] Arr, ref int Minx)
-        {
-            Minx = Int32.MaxValue;
-            for (int i = 0; i < Arr.Length; i++)
-            {
-                if (Arr[i] < Minx)
+        {         
+                if(Arr.Length > 1)
                 {
-                    Minx = Arr[i];
+                   // Minx = Int32.MaxValue;
+                    for (int i = 0; i < Arr.Length; i++)
+                    {
+                        if (Arr[i] < Minx)
+                        {
+                            Minx = Arr[i];
+                        }
+                    }
+                    Console.WriteLine(Minx);
                 }
-            }
-            Console.WriteLine(Minx);
+                else
+                {
+                    Console.WriteLine("Array not exit emty");
+                }
             Console.ReadLine();
         }
 
